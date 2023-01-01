da85c737240e85cf1173c45cc1ad2b615482c250
CheonYoungJo <pknb213@naver.com>
2023-01-02 01:15:16 +0900

    Added account

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"./account"
 	"fmt"
 	"strings"
 )
@@ -33,18 +34,72 @@ func superAdd(numbers ...int) (total int) {
 }
 
 func canIDrking(age int) bool {
-	if age < 18 {
-		return false
+	if age > 20 {
+		switch koreanAge := age + 2; koreanAge {
+		case 10:
+			return false
+		case 18:
+			return true
+		}
 	}
 	return true
 }
 
+func pointers(val int) {
+	addr := &val
+	fmt.Println(val, addr, &val, &addr, *addr)
+}
+
+func array() {
+	name := [5]string{"a", "b", "C"}
+	name[3] = "d"
+	name[4] = "f"
+	fmt.Println(name)
+
+	slice := []string{"S", "A"}
+	slice = append(slice, "B", "C")
+	fmt.Println(slice)
+}
+
+func mapTest() {
+	me := map[string]string{"LastName": "Cheon", "FirstName": "YJ"}
+	for k, v := range me {
+		fmt.Println(k, v)
+	}
+	fmt.Println(me)
+}
+
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
+func structTest() {
+	food := []string{"Rice", "Egg", "Sosigy"}
+	yj := person{"cyj", 32, food}
+	fmt.Println(yj)
+
+}
+
 func main() {
-	fmt.Println("Hello world")
-	fmt.Println(multiply(7, 7))
-	totalLength, upperName := lenAndUpper("Cheon")
-	fmt.Println(totalLength, upperName)
-	repeatMe("Cheon", "Young", "Jo", "Goods")
-	fmt.Println(superAdd(1, 2, 3, 4, 5, 6, 7))
-	fmt.Println(canIDrking(16))
+	//fmt.Println("Hello world")
+	//fmt.Println(multiply(7, 7))
+	//totalLength, upperName := lenAndUpper("Cheon")
+	//fmt.Println(totalLength, upperName)
+	//repeatMe("Cheon", "Young", "Jo", "Goods")
+	//fmt.Println(superAdd(1, 2, 3, 4, 5, 6, 7))
+	//fmt.Println(canIDrking(16))
+	//pointers(7)
+	//array()
+	//mapTest()
+	//structTest()
+	account := account.NewAccount("CYJ")
+	account.Deposit(1000)
+	fmt.Println(account)
+	err := account.Withdraw(2000)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance())
 }
