3df574dcef1be3b8c14edf5c5dfa2ed818fdee2b
CheonYoungJo <pknb213@naver.com>
2023-01-05 01:49:50 +0900

    Updated Code

--- project/main.go
+++ project/main.go
@@ -4,11 +4,13 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
+	var results = make(map[string]string)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -21,15 +23,33 @@ func main() {
 		"https://academy.nomadcoders.co/",
 	}
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
 	}
+	for url, res := range results {
+		fmt.Println(url, res)
+	}
+	go sexyCount("Y")
+	go sexyCount("J")
+	time.Sleep(time.Second * 5)
 }
 
 func hitURL(url string) error {
 	fmt.Println("Checking :", url)
 	resp, err := http.Get(url)
-	if err == nil || resp.StatusCode >= 400 {
+	if err != nil || resp.StatusCode >= 400 {
 		return errRequestFailed
 	}
 	return nil
+}
+
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
+	}
 }
